f0ab5933ed0c6018b7ba9aad13fd4e460f101bf7
Christian Rijke <hello@christianrijke.de>
2025-03-14 15:48:53 +0100

    Use dynamic imports.

--- apps/shell/src/AppRoute.tsx
+++ apps/shell/src/AppRoute.tsx
@@ -1,5 +1,4 @@
 import { createRemoteComponent } from "@module-federation/bridge-react";
-import { init, loadRemote } from "@module-federation/enhanced/runtime";
 import {
   createBrowserRouter,
   Navigate,
@@ -8,47 +7,23 @@ import {
 } from "react-router-dom";
 import Shell from "./Shell";
 
-init({
-  name: "shell",
-  remotes: [
-    {
-      name: "app1",
-      alias: "app1",
-      entry: "http://localhost:5174/remoteEntry.js",
-      shareScope: "react@18.3.0",
-    },
-    {
-      name: "app2",
-      alias: "app2",
-      entry: "http://localhost:5175/remoteEntry.js",
-      shareScope: "react@18.3.0",
-    },
-    {
-      name: "app3",
-      alias: "app3",
-      entry: "http://localhost:5176/remoteEntry.js",
-      shareScope: "react@18.3.1",
-    },
-  ],
-});
-
 const App1Component = createRemoteComponent({
   // @ts-ignore
-  loader: () => loadRemote("app1/export-app"),
+  loader: () => import("app1/export-app"),
   loading: <h1>app-1 loading</h1>,
   fallback: () => <h1>app-1 error</h1>,
 });
 
 const App2Component = createRemoteComponent({
   // @ts-ignore
-  loader: () => loadRemote("app2/export-app"),
+  loader: () => import("app2/export-app"),
   loading: <h1>app-2 loading</h1>,
   fallback: () => <h1>app-2 error</h1>,
 });
 
 const App3Component = createRemoteComponent({
   // @ts-ignore
-  loader: () => loadRemote("app3/export-app"),
+  loader: () => import("app3/export-app"),
   loading: <h1>app-3 loading</h1>,
   fallback: () => <h1>app-3 error</h1>,
 });
